c9df3c3bdd1d702260d678498ee8235cd0283a35
TanveetGill <tanveet.gill@gmail.com>
2016-06-12 17:28:10 -0400

    database setup

--- myapp/models/database.js
+++ myapp/models/database.js
@@ -1,5 +1,69 @@
-var pg = require('pg');
-var connectionString = 'postgres://localhost:5432/livabetic';
+var Sequelize = require('sequelize');
 
-var client = new pg.Client(connectionString);
-client.connect();
+var connection = new Sequelize('livabetic', 'tanny', 'tatti123', {
+	host: 'localhost',
+	dialect: 'postgres',
+
+	pool: {
+		max: 5,
+		min: 0,
+		idle: 10000
+	}
+});
+
+connection
+	.authenticate()
+	.then(function(err) {
+		console.log('Connection has been established successfully.');
+	})
+	.catch(function(err) {
+		console.log('Unable to connect to the database:', err);
+	});
+
+var User = connection.define('user', {
+	firstName: {
+		type: Sequelize.STRING,
+		field: 'first_name',
+		validate: {
+			notNull: true
+		}
+	},
+	lastName: {
+		type: Sequelize.STRING,
+		field: 'last_name',
+		validate: {
+			notNull: true
+		}
+	},
+	email: {
+		type: Sequelize.STRING,
+		isEmail: true,
+		unique: true,
+		validate: {
+			notNull: true
+		}
+	},
+	password: {
+		type: Sequelize.STRING,
+		validate: {
+			len: [8, 50],
+			notNull: true
+		}
+	}
+});
+
+connection
+	.sync({
+		force: true
+	})
+	.then(function() {
+		var req = {
+			user_params: {
+				firstname: ,
+				lastname: ,
+				email: ,
+				password:
+			} 
+		}
+		User.create(req,user_params)
+	});
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,3 +1,4 @@
+var models = require('../models/database')
 var express = require('express');
 var router = express.Router();
 
