81e83e835c5bb1f5f32f125815a7849d19e448c3
dsomel21 <dsomel21@gmail.com>
2016-06-17 17:29:04 -0400

    Added some useful comments, and parsed in the BWZ Correction column too

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -12,39 +12,44 @@ var router = express.Router();
 var passport = require('passport');
 var csv = require('fast-csv');
 var multer = require('multer');
+/* Commented out in case we need to store the data in a folder */
 // var upload = multer({ dest: 'uploads/' }).single('csvdata')
 var upload = multer({ inMemory: true }).single('csvdata');
 
-// ALL ROUTES
+/* All Routes */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Livabetic' })
+	res.render('index', { title: 'Livabetic' })
 });
 
 router.post('/upload/data', upload, function(req, res) {
-  csv.fromString(req.file.buffer.toString(), {headers : true })
-    .on("data", function(data){
-      console.log('.');
+	csv.fromString(req.file.buffer.toString(), {headers : true })
+	.on("data", function(data){
+		/* For debugging::: to verify it's executing line by line*/
+      // console.log('.');
 
-      if (data['Daily Insulin Total (U)'] > 0) {
-        console.log('------------------------------------------------------------------------------------------------');
-      }
-
-      // if (data['BWZ Insulin Sensitivity (mmol/L)'] > 0){
-      console.log('Index:', data['Index'], 'Date:', data['Date'],
-                  'Time:',
-                  data['Time'], 'BG Reading (mmol/L):',
-                  data['BG Reading (mmol/L)'], 'BWZ Estimate (U):',
-                  data['BWZ Estimate (U)'], 'BWZ Carb Input (grams)',
-                  data['BWZ Carb Input (grams)'], 'Daily Insulin Total (U):',
-                  'BWZ Insulin Sensitivity (mmol/L):', data['BWZ Insulin Sensitivity (mmol/L)']);
-      // data['Daily Insulin Total (U)']);
+      /* For debugging::: to distinting days */
+      // if (data['Daily Insulin Total (U)'] > 0) {
+      //   console.log('------------------------------------------------------------------------------------------------');
       // }
-	})
+
+      console.log(
+      	/* For debugging::: verifying we have the right data*/
+      						// 'Index:', data['Index'], 
+      						'Date:', data['Date'],
+      						'Time:', data['Time'], 
+      						// 'BG Reading (mmol/L):', data['BG Reading (mmol/L)'],
+      						// 'BWZ Estimate (U):', data['BWZ Estimate (U)'],
+      						// 'BWZ Carb Input (grams)',data['BWZ Carb Input (grams)'],
+      						// 'BWZ Insulin Sensitivity (mmol/L):', data['BWZ Insulin Sensitivity (mmol/L)'],
+      						'BWZ Correction Estimate (U):', data['BWZ Correction Estimate (U)'],
+      						'Daily Insulin Total (U):', data['Daily Insulin Total (U)']
+      						);
+    })
 	.on("end", function(){
-      console.log("[Completed]");
+		console.log("[Completed]");
       // console.log(dilraj);
       res.send("Yay!");
-	});
+    });
 
 });
 
@@ -53,9 +58,9 @@ router.get('/login', function(req, res, next) {
 });
 
 router.post('/login', passport.authenticate('local-login', {
-  successRedirect : '/dashboard',
-  failureRedirect : '/',
-  failureFlash : true
+	successRedirect : '/dashboard',
+	failureRedirect : '/',
+	failureFlash : true
 }));
 
 router.get('/signup', function(req, res, next) {
@@ -63,11 +68,11 @@ router.get('/signup', function(req, res, next) {
 });
 
 router.post('/signup', passport.authenticate('local-signup', {
-  successRedirect : '/dashboard',
-  failureRedirect : '/',
-  failureFlash : true
+	successRedirect : '/dashboard',
+	failureRedirect : '/',
+	failureFlash : true
 })
-           )
+)
 
 // router.post('/signup', (req, res) => {
 // 	var user_params = {
@@ -82,7 +87,7 @@ router.post('/signup', passport.authenticate('local-signup', {
 // });
 
 router.get('/dashboard', isLoggedIn, function(req, res) {
-  res.render('dashboard')
+	res.render('dashboard')
 });
 
 router.get('/logout', function(req, res) {
@@ -98,7 +103,7 @@ router.get('/logout', function(req, res) {
 // path and callback function, this acts like middleware to check if user is authenticated already
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) // if user is authenticated in the session carry on and no need to resign them in
-    return next();
+  	return next();
   res.redirect('/'); // if they are not already logged in then we will redirect them to the homepage
 }
 
